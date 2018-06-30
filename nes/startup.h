b06178dbe020cba534d7174532ff24c3aa51b303
Ibrahim Odeh <irodeh2@illinois.edu>
2018-06-29 19:43:45 -0500

    added some header file function declarations

--- nes/startup.h
+++ nes/startup.h
@@ -1,1 +1,16 @@
 int call_instruction();
+int immediate();
+int zp_abs();
+int absolute();
+int zp_indexX();
+int zp_indexY();
+int abs_indX();
+int abs_indY();
+int indirX();
+int indirY();
+int accumulator();
+int branch();
+int X8();
+int X0();
+int XA();
+int call(); //NOTE: might not use this
